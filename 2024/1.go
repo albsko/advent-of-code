3a38efed8210aa28d0eec306adcb7418a6659578
Albert Skonieczny <askonieczny@fandom.com>
2024-12-06 17:44:05 +0100

    feat(y2024_1): yay

--- 2024/1.go
+++ 2024/1.go
@@ -1,7 +1,46 @@
-package y2024
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
-func main() {
-	fmt.Println("advent of code 2024")
+func y2024_1() int {
+	f, _ := os.Open("1.txt")
+	defer f.Close()
+
+	left := make([]int, 0, 1000)
+	right := make([]int, 0, 1000)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		s := strings.Split(line, ",")
+
+		l, _ := strconv.Atoi(s[0])
+		r, _ := strconv.Atoi(s[1])
+
+		left = append(left, l)
+		right = append(right, r)
+	}
+
+	sort.Ints(left)
+	sort.Ints(right)
+
+	abs := func(x int) int {
+		if x < 0 {
+			return -x
+		}
+		return x
+	}
+
+	result := 0
+	for i := range left {
+		result = result + abs(left[i]-right[i])
+	}
+
+	return result
 }
---
+++ 2024/1_test.go
@@ -0,0 +1,8 @@
+package main
+
+import "testing"
+
+func TestY2024_1(t *testing.T) {
+	result := y2024_1()
+	t.Log(result)
+}
